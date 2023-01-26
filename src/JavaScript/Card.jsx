cfd6a6422f8f3f10327c11e9549e4d84f2231821
Bubbl3Waffl3 <salahlfd0@gmail.com>
2023-01-26 14:23:26 +0100

    marca de agua quitada

--- src/JavaScript/Card.jsx
+++ src/JavaScript/Card.jsx
@@ -2,18 +2,18 @@ import React, { useState } from 'react'
 import reverseCard from "../img/reversoCarta.png";
 
 function Card(props) {
-    let [reverseStyle, setReverseStyle] = useState({display: "block"})
-    let [frontStyle, setFrontStyle] = useState({display: "none"})
+    let [reverseStyle, setReverseStyle] = useState({ display: "block" })
+    let [frontStyle, setFrontStyle] = useState({ display: "none" })
     let [mouseOver, setMouseOver] = useState("")
-    
+
 
     function turnCard() {
         setReverseStyle(frontStyle)
         setFrontStyle(reverseStyle)
-    } 
-    
-    function overCard(event){
-        setMouseOver ( (event.type === "mouseover") ? props.meaning : "")
+    }
+
+    function overCard(event) {
+        setMouseOver((event.type === "mouseover") ? props.meaning : "")
     }
 
 
@@ -21,7 +21,7 @@ function Card(props) {
         <div className='main-cardGroup-card'>
             <p style={frontStyle}>{props.name}</p>
             <img src={reverseCard} onClick={turnCard} style={reverseStyle} alt="reverse Card" />
-            <img src={props.image} onMouseOut={(event)=>{overCard(event)}} onMouseOver={(event) => {overCard(event)}} style={frontStyle} alt="reverse Card" />
+            <img src={props.image} onMouseOut={(event) => { overCard(event) }} onMouseOver={(event) => { overCard(event) }} style={frontStyle} alt="reverse Card" />
             <p>{mouseOver}</p>
             <p>{props.cardType}</p>
         </div>
