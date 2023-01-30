620efe74d6fd61d7f30c1abcf5cdb23e9c7cbce6
perezdh13@gmail.com <Perezdh13@gmail.com>
2023-01-30 17:46:47 +0100

    css main page

--- src/JavaScript/MainPage.jsx
+++ src/JavaScript/MainPage.jsx
@@ -1,13 +1,35 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import deckCards from "../img/mazoCartas.png";
+import reverseCard from "../img/reversoCarta.png";
 
 function MainPage() {
 
 
-    
+  function seeCards() {
+    let deckCards = []
+    for (let i = 0; i < 18; i++) {
+      deckCards.push(reverseCard)
+    }
+    return deckCards
+  }
+
+  console.log(seeCards());
+
+
   return (
-    <div>
-    <Link to="/prediction"><button>pulsame</button></Link>
+    <div className='main-page'>
+      <h1>Escoje tus cartas</h1>
+      <div className='main-page-cards'>
+        {seeCards().map((item) => {
+          return <img className='main-page-cards-img' src={item}></img>
+        })}
+      </div>
+      <div className='main-page-deck'>
+        <Link to="/prediction">
+          <img className='main-page-deck-img' src={deckCards}></img>
+        </Link>
+      </div>
     </div>
   )
 }
