a74ed620e339758528454dc1c798049ba14494ac
perezdh13@gmail.com <Perezdh13@gmail.com>
2023-01-31 14:48:37 +0100

    main page choosing cards

--- src/JavaScript/MainPage.jsx
+++ src/JavaScript/MainPage.jsx
@@ -5,29 +5,40 @@ import reverseCard from "../img/reversoCarta.png";
 
 function MainPage() {
 
+let [choseCardStyle, setChoseCardStyle] = useState(Array(18).fill({display:"block"})) 
+
+function chooseCards(index){
+  let newChoseCardStyle = [...choseCardStyle];
+    newChoseCardStyle[index] = {display: "block", opacity:"0.5"};
+    setChoseCardStyle(newChoseCardStyle)
+    console.log(newChoseCardStyle);
+}
+
 
   function seeCards() {
     let deckCards = []
     for (let i = 0; i < 18; i++) {
-      deckCards.push(reverseCard)
+      deckCards.push([reverseCard])
     }
+   console.log(deckCards);
     return deckCards
   }
 
-  console.log(seeCards());
-
 
   return (
-    <div className='main-page'>
+    <div className='main-page' >
       <h1>Escoje tus cartas</h1>
       <div className='main-page-cards'>
-        {seeCards().map((item) => {
-          return <img className='main-page-cards-img' src={item}></img>
-        })}
+        {seeCards().map((item,index) => {
+          return (
+          <div id={index} className='main-page-cards-img'>
+         <img id={index} src={reverseCard} onClick={() => chooseCards(index)} style={choseCardStyle[index]} alt="reverse card" />
+          </div>
+        )})}
       </div>
       <div className='main-page-deck'>
         <Link to="/prediction">
-          <img className='main-page-deck-img' src={deckCards}></img>
+          <img className='main-page-deck-img' src={deckCards} alt="reverse card" />
         </Link>
       </div>
     </div>
